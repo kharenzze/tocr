0ba5ebeeefa3afe093516947ff4662f73eb7d586
Paolo Ráez <str237@gmail.com>
2023-01-22 10:50:53 +0100

    File input. Convert to URL.

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,32 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
+  import Greet from "./lib/Greet.svelte";
+
+  const getDataUrl = async (file) => {
+    const fReader = new FileReader();
+    fReader.readAsDataURL(file);
+    const readPromise = new Promise((resolve, reject) => {
+      fReader.onloadend = (evt) => resolve(evt.target.result);
+    });
+    return readPromise;
+  };
+
+  const inputHandler = async (evt) => {
+    const file = evt.target.files[0];
+    const url = await getDataUrl(file);
+    console.log(url);
+  };
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri!</h1>
+  <h1>Welcome to TOCR!</h1>
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
+  <input type="file" on:change={inputHandler} />
 
   <div class="row">
     <Greet />
   </div>
-
-
 </main>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
 </style>
+
