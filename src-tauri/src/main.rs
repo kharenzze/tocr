a62fcdea1f34fa6bcadaa175d707aa316d66b619
Paolo Ráez <str237@gmail.com>
2023-02-03 10:18:55 +0100

    Image processing + return string

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,23 +5,22 @@
 
 //extern crate base64;
 //extern crate leptess;
-
-use base64::{
-    alphabet,
-    engine::{self, general_purpose},
-    Engine as _,
-};
+//
+use base64::{engine::general_purpose, Engine};
+use leptess::{leptonica, tesseract};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn process_image(url: &str) -> String {
-    println!("Here!");
     let base64_data = url.trim_start_matches("data:image/png;base64,");
     let bytes = general_purpose::STANDARD.decode(base64_data).unwrap();
-    format!(
-        "Hello, {}! You've been greeted from Rust!",
-        &base64_data[0..10]
-    )
+
+    let mut api = tesseract::TessApi::new(None, "eng").unwrap();
+    let pix = leptonica::pix_read_mem(&bytes).unwrap();
+    api.set_image(&pix);
+
+    let text = api.get_utf8_text();
+    text.unwrap()
 }
 
 fn main() {
